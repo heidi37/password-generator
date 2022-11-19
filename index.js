0c5862b4e7dc4ba918f0e6569884ef05e0678bb2
Heidi Fryzell <hfryzell@gmail.com>
2022-11-19 16:59:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let btn = document.querySelector('.btn')
+
+let firstPass = document.getElementById('firstPassword')
+let secondPass = document.getElementById('secondPassword')
+
+
+btn.addEventListener("click", function () {
+  generatePasswords()
+ })
+ 
+ function generatePasswords() {
+     function randomIndex() { 
+         return Math.floor(Math.random() * characters.length)
+         }
+     for (let i = 0; i < 15; i++){
+         firstPass.textContent += characters[randomIndex()]
+         secondPass.textContent += characters[randomIndex()]
+     } 
+ }
+
+
+
